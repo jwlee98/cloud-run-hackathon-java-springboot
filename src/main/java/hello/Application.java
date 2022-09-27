727d09eca7755918c76b36b71e79fc3023006889
jwlee98 <jwlee98@gmail.com>
2022-09-27 05:54:47 +0000

    dont shoot

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -59,28 +59,29 @@ public class Application {
 
     String[] moves = new String[]{"F", "R", "F", "L"};
     int i = new Random().nextInt(4);
-    String[] commands = new String[]{"F", "R", "T", "T", "T", "T", "T", "T", "T", "T", "F", "R","T", "T", "T", "T", "T", "T", "T", "T"};
-    int j = new Random().nextInt(20);
-    String[] command2 = new String[]{"R", "T", "T", "T", "T", "T", "T", "T", "T"};
-    int k = new Random().nextInt(9);
+    //String[] commands = new String[]{"F", "R", "T", "T", "T", "T", "T", "T", "T", "T", "F", "R","T", "T", "T", "T", "T", "T", "T", "T"};
+    //int j = new Random().nextInt(20);
+    //String[] command2 = new String[]{"R", "T", "T", "T", "T", "T", "T", "T", "T"};
+    //int k = new Random().nextInt(9);
 
-    PlayerState playstate01 = arenaUpdate.arena.state.get("https://cloud-run-hackathon-java-springboot-4dnmcw5raa-uc.a.run.app");
+    //PlayerState playstate01 = arenaUpdate.arena.state.get("https://cloud-run-hackathon-java-springboot-4dnmcw5raa-uc.a.run.app");
     
     // TODO add your implementation here to replace the random response. 
     //if(playstate01.wasHit == true)
     //    return moves[i];
     //else
 
-    if((playstate01.x == 4 || playstate01.x == 5 || playstate01.x == 6) && playstate01.y == 3)
-        {
-            System.out.println("score: " + playstate01.score + " wasHit: " + playstate01.wasHit + " x: " + playstate01.x + " y: " + playstate01.y);
-            return command2[k];
-        } 
-    else
-        {
-            System.out.println("score: " + playstate01.score + " wasHit: " + playstate01.wasHit + " x: " + playstate01.x + " y: " + playstate01.y);
-            return commands[j];
-        }
+    //if((playstate01.x == 4 || playstate01.x == 5 || playstate01.x == 6) && playstate01.y == 3)
+    //    {
+    //        System.out.println("score: " + playstate01.score + " wasHit: " + playstate01.wasHit + " x: " + playstate01.x + " y: " + playstate01.y);
+    //        return command2[k];
+    //    } 
+    //else
+    //    {
+    //        System.out.println("score: " + playstate01.score + " wasHit: " + playstate01.wasHit + " x: " + playstate01.x + " y: " + playstate01.y);
+    //        return commands[j];
+    //    }
+    return moves[i];
     
   }
 
