8bdba9d74cad0266becfb620274230bfb06506e6
jwlee98 <jwlee98@gmail.com>
2022-09-27 04:32:41 +0000

    test05

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -59,8 +59,10 @@ public class Application {
 
     //String[] moves = new String[]{"F", "R", "F", "L"};
     //int i = new Random().nextInt(4);
-    String[] commands = new String[]{"F", "R", "T", "T", "T", "T", "T", "T", "T", "T", "F", "L","T", "T", "T", "T", "T", "T", "T", "T"};
+    String[] commands = new String[]{"F", "R", "T", "T", "T", "T", "T", "T", "T", "T", "F", "R","T", "T", "T", "T", "T", "T", "T", "T"};
     int j = new Random().nextInt(20);
+    String[] command2 = new String[]{"R", "T", "T", "T", "T", "T", "T", "T", "T"};
+    int k = new Random().nextInt(9);
 
     PlayerState playstate01 = arenaUpdate.arena.state.get("https://cloud-run-hackathon-java-springboot-4dnmcw5raa-uc.a.run.app");
     System.out.println("score: " + playstate01.score + " wasHit: " + playstate01.wasHit);
@@ -69,7 +71,12 @@ public class Application {
     //if(playstate01.wasHit == true)
     //    return moves[i];
     //else
+
+    if(playstate01.x == 7 && playstate01.y == 5)
+        return command2[k];
+    else
         return commands[j];
+    
   }
 
 }
